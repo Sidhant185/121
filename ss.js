92a202b29a675744100270741c6d71e16550eb11
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2021-01-07 06:57:23 -0800

    Add files via upload

---
+++ ss.js
@@ -0,0 +1,18 @@
+class ss {
+    constructor(bodyA,bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffeness: 0.04,
+            length:10
+        }
+        this.sling = Constraint.create(options)
+        World.add(world,this.sling)
+    
+      
+    }
+    display(){
+        strokeWeight(10); 
+        line(this.sling.bodyA.position.x,this.sling.bodyA.position.y,this.sling.bodyB.position.x,this.sling.bodyB.position.y)
+    }
+}
